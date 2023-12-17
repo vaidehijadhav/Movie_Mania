572ace2675bfee9a87ea75f32624ab30c5a3d26e
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2023-12-18 01:50:43 +0530

    write logic for empty boxes

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ function sendEmail(){
         name_error_message: document.getElementById("name_error_message"),    
     };
 
+    param.name_error_name.innerHTML = "";
+    param.name_error_email.innerHTML = "";
+    param.name_error_message.innerHTML = "";
+
+    if (param.fname === "") {
+        param.name_error_name.innerHTML = "Please fill out the name";
+        return; // Stop form submission if any field is empty
+    }
+
+    if (param.email === "") {
+        param.name_error_email.innerHTML = "Please fill out the email";
+        return; // Stop form submission if any field is empty
+    }
+
+    if (param.message === "") {
+        param.name_error_message.innerHTML = "Please fill out the message";
+        return; // Stop form submission if any field is empty
+    }
+
 
     const serviceID = 'service_5wsh1h2';
     const templateID = 'template_p1hav18';
