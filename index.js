db0820852cb4c692f37bd8471a8b608ec3b79038
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2023-12-18 00:58:56 +0530

    upadate email

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function sendEmail(){
         name_error_email: document.getElementById("name_error_email"),
         name_error_message: document.getElementById("name_error_message"),    
     };
+
+
+    const serviceID = 'service_5wsh1h2';
+    const templateID = 'template_p1hav18';
+
+    emailjs.send(serviceID, templateID, param)
+    .then(function(res){
+        console.log(res);
+        alert("Thank you for contacting us!");
+        // document.getElementById('contactForm').reset();
+        param.form.reset();
+        }, function(error) {
+        console.log("Email failed to send:", error);
+        alert("Oops! Something went wrong. Please try again later.");
+    });
 }
